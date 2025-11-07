b04f5c66a79083cab571c819509cd60eb759241e
yukito <yukito@inoueshoutonoMacBook-Air.local>
2025-11-07 14:49:48 +0900

    project start

---
+++ src/app/about/page.js
@@ -0,0 +1,7 @@
+export default function ABOUT() {
+  return (
+    <>
+      aboutページです。
+    </>
+  );
+}
---
+++ src/app/not-found.js
@@ -0,0 +1,7 @@
+export default function notFount() {
+  return (
+    <>
+      <p>ページないよ</p>
+    </>
+  );
+}
