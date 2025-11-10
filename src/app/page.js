e038234038a57431a88ccbdec385aa81f2a0487c
yukito <yukito@inoueshoutonoMacBook-Air.local>
2025-11-10 13:24:04 +0900

    three.js installed

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Link from "next/link";
+// import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-      <p><Link href={"/about"}>about</Link></p>
-      <p><Link href={"/test"}>test</Link></p>
+      {/* <p><Link href={"/about"}>about</Link></p>
+      <p><Link href={"/test"}>test</Link></p> */}
     </>
   );
 }
