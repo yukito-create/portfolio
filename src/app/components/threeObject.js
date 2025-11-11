ac0fc56e474f8eea24f2c612f4e07ba7f198c03f
yukito <yukito@inoueshoutonoMacBook-Air.local>
2025-11-11 14:11:35 +0900

    box created

---
+++ src/app/components/threeObject.js
@@ -0,0 +1,80 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+// three.jsオブジェクトのcanvasコンポーネント
+export default function ThreeObject() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    /**
+     * 描画に必要な3要素の作成
+     */
+    // 1.レンダラーを作成
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+    });
+    
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    
+    // 2.シーンを作成
+    const scene = new THREE.Scene();
+    
+    // 3.カメラを作成
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      sizes.width / sizes.height,
+      1,
+      10000
+    );
+    
+    camera.position.set(0, 0, 1000);
+    scene.add(camera);
+    
+    /**
+     * オブジェクトの作成
+     */
+    const boxGeometry = new THREE.BoxGeometry(250, 250, 250);
+    const material = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+    });
+    const box = new THREE.Mesh(boxGeometry, material);
+
+    scene.add(box);
+
+    /**
+     * ライトの作成
+     */
+    const directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.intensity = 4;
+    directionalLight.position.set(1, 1, 1);
+
+    scene.add(directionalLight);
+
+    /**
+     * アニメーション
+     */
+    tick();
+
+    function tick() {
+      requestAnimationFrame(tick);
+
+      box.rotation.x += 0.01;
+      box.rotation.y += 0.01;
+
+      // レンダリング
+      renderer.render(scene, camera);
+    }
+  }, []);
+
+  return <canvas ref={canvasRef}></canvas>;
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 // import Link from "next/link";
 import styles from "./page.module.css";
+import ThreeObject from "./components/threeObject.js";
 
 export default function Home() {
   return (
     <>
-      {/* <p><Link href={"/about"}>about</Link></p>
-      <p><Link href={"/test"}>test</Link></p> */}
+      <ThreeObject />
     </>
   );
 }
