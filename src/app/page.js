a59b475ae0827d619f9a5e5cff0f212647b71866
yukito <yukito@inoueshoutonoMacBook-Air.local>
2025-11-12 14:38:55 +0900

    toppage provisionally completed.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,22 @@
-// import Link from "next/link";
+import Link from "next/link";
 import styles from "./page.module.css";
 import ThreeObject from "./components/threeObject.js";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ weight: ["600"] });
 
 export default function Home() {
   return (
     <>
       <ThreeObject />
+      <nav className={styles.nav}>
+        <ul>
+          <li className={montserrat.className}><Link href={"/about"}>ABOUT</Link></li>
+          <li className={montserrat.className}><Link href={"/works"}>WORKS</Link></li>
+          <li className={montserrat.className}><Link href={"/blog"}>BLOG</Link></li>
+          <li className={montserrat.className}><Link href={"/contack"}>CONTACT</Link></li>
+        </ul>
+      </nav>
     </>
   );
 }
