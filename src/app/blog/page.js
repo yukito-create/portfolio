b7db95f0ab899b8ee3d125542896266f876f3dba
yukito <yukito@inoueshoutonoMacBook-Air.local>
2025-11-14 15:51:09 +0900

    completed.

---
+++ src/app/blog/page.js
@@ -0,0 +1,17 @@
+export default function ABOUT() {
+  const preparation = {
+    color: "#fff",
+    fontSize: "3rem",
+    backgroundColor: "#000",
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  return (
+    <>
+      <div style={preparation}>準備中です😃</div>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
           <li className={montserrat.className}><Link href={"/about"}>ABOUT</Link></li>
           <li className={montserrat.className}><Link href={"/works"}>WORKS</Link></li>
           <li className={montserrat.className}><Link href={"/blog"}>BLOG</Link></li>
-          <li className={montserrat.className}><Link href={"/contack"}>CONTACT</Link></li>
+          <li className={montserrat.className}><Link href={"/contact"}>CONTACT</Link></li>
         </ul>
       </nav>
     </>
